46e5887ffc0235553528fc6c3ab382ea26bdd33c
KhayThuk <muoupop@gmail.com>
2026-02-01 14:46:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(productId) {
+    var details = document.getElementById(productId);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
