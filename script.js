2873bf3615c9489dac1929ea90ad03f7d5104f7f
KhayThuk <muoupop@gmail.com>
2026-03-02 09:40:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDetails(productId) {
-    var details = document.getElementById(productId);
-    if (details.style.display === "none" || details.style.display === "") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
